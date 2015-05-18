28209826154f2fdb3d907d5ae0b6b74be3aab5a8
ivo <ivostoianov94@gmail.com>
2015-05-18 17:32:00 +0300

    Add login functionality

--- scripts/controllers/authenticationController.js
+++ scripts/controllers/authenticationController.js
@@ -1,5 +1,16 @@
 /* global socialNetwork */
 
-socialNetwork.controller('authenticationController', function ($scope) {
-
+socialNetwork.controller('authenticationController', function ($scope, userAuthentication) {
+    $scope.login = function () {
+        userAuthentication.login($scope.loginData)
+            .then(function (data) {
+                //future: replace with noty
+                alert('Succsesful login. Username: (' + data.userName + ')');
+                userAuthentication.saveCredentials(data);
+            }, function (error) {
+                //future: replace with noty
+                alert(error.error_description);
+                console.log(error);
+            });
+    };
 });
