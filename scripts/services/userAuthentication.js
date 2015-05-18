9feec263a4993e86bbdacb4c32117e1cda0cb882
ivo <ivostoianov94@gmail.com>
2015-05-18 15:29:50 +0300

    Create user login and register service

--- scripts/services/userAuthentication.js
+++ scripts/services/userAuthentication.js
@@ -1,1 +1,48 @@
 /* global socialNetwork */
+
+socialNetwork.factory('UserAuthentication', function ($http, $q, BASE_URL) {
+    var requester = {};
+
+    requester.register = function (username, password, confirmPassword, name, email) {
+        //changes: get the data as parameter?
+        var data = {
+            username: username,
+            password: password,
+            confirmPassword: confirmPassword,
+            name: name,
+            email: email
+        };
+
+        var url = BASE_URL + 'users/register';
+
+        var deferred = $q.defer();
+
+        $http.post(url, data)
+            .success(function (data) {
+                deferred.resolve(data);
+            })
+            .error(function (data) {
+                deferred.reject(data);
+            });
+    };
+
+    requester.login = function (username, password) {
+        //changes: get the data as parameter?
+        var data = {
+            username: username,
+            password: password
+        };
+
+        var url = BASE_URL + 'users/login';
+
+        var deferred = $q.defer();
+
+        $http.post(url, data)
+            .success(function (data) {
+                deferred.resolve(data);
+            })
+            .error(function (data) {
+                deferred.reject(data);
+            });
+    };
+});
