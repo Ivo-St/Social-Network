f952e13c6f448cd21323d7e4c032b69d86fc0f63
ivo <ivostoianov94@gmail.com>
2015-05-18 22:20:10 +0300

    Add logout and clearCredentials functions

--- scripts/services/userAuthentication.js
+++ scripts/services/userAuthentication.js
@@ -34,9 +34,36 @@ socialNetwork.factory('userAuthentication', function ($http, $q, BASE_URL) {
         return deferred.promise;
     };
 
+    userService.logout = function () {
+        var deferred = $q.defer();
+
+        var request = {
+            method: 'POST',
+            url: BASE_URL + 'users/logout',
+            headers: {
+                Authorization: 'Bearer ' + sessionStorage.accessToken
+            }
+        };
+
+        $http(request)
+            .success(function (data) {
+                deferred.resolve(data);
+            })
+            .error(function (data) {
+                deferred.reject(data);
+            });
+
+        return deferred.promise;
+    };
+
     userService.saveCredentials = function (data) {
         sessionStorage.username = data.userName;
         sessionStorage.accessToken = data.access_token;
+    };
+
+    userService.clearCredentials = function () {
+        sessionStorage.removeItem('username');
+        sessionStorage.removeItem('accessToken');
     };
 
     return userService;
