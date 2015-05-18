ffe36913db7b9d64427be423944cbc16bcd58f9b
ivo <ivostoianov94@gmail.com>
2015-05-18 15:29:00 +0300

    Add base url constant

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,9 @@
 
 var socialNetwork = angular.module('SocialNetwork', ['ngRoute']);
 
+//future: move the constants to another file
+socialNetwork.constant('BASE_URL', 'http://softuni-social-network.azurewebsites.net/api/');
+
 socialNetwork.config(function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'partials/wellcome.html'
