84d25cb570634d81e5727887dbd455c1760ece2d
ivo <ivostoianov94@gmail.com>
2015-05-19 16:34:07 +0300

    Add functions to get the user's profile data

--- scripts/controllers/authenticationController.js
+++ scripts/controllers/authenticationController.js
@@ -1,14 +1,19 @@
-/* global socialNetwork */
+/* global socialNetwork, sessionStorage */
 
-socialNetwork.controller('authenticationController', function ($scope, userAuthentication) {
+socialNetwork.controller('authenticationController', function ($scope, $q, userAuthentication) {
     $scope.login = function () {
         userAuthentication.login($scope.loginData)
             .then(function (data) {
-                //future: replace with noty
+
+                // future: replace with noty
                 alert('Succsesful login. Username: (' + data.userName + ')');
                 userAuthentication.saveCredentials(data);
+
+                // changes: see if this can be loaded in a better place
+                $scope.getOwnProfileData();
             }, function (error) {
-                //future: replace with noty
+
+                // future: replace with noty
                 alert(error.error_description);
                 console.log(error);
             });
@@ -17,11 +22,16 @@ socialNetwork.controller('authenticationController', function ($scope, userAuthe
     $scope.register = function () {
         userAuthentication.register($scope.registerData)
             .then(function (data) {
-                //future: replace with noty
+
+                // future: replace with noty
                 alert('Succsesful register. Username: (' + data.userName + ')');
                 userAuthentication.saveCredentials(data);
+
+                // changes: see if this can be loaded in a better place
+                $scope.getOwnProfileData();
             }, function (error) {
-                //future: replace with noty
+
+                // future: replace with noty
                 alert(error.message);
                 console.log(error);
             });
@@ -30,12 +40,32 @@ socialNetwork.controller('authenticationController', function ($scope, userAuthe
     $scope.logout = function () {
         userAuthentication.logout()
             .then(function (data) {
-                //future: replace with noty
+
+                // future: replace with noty
                 alert(data.message);
                 userAuthentication.clearCredentials();
             }, function (data) {
-                //future: replace with noty
+
+                // future: replace with noty
                 alert(data.message);
             });
+    };
+
+    $scope.getOwnProfileData = function () {
+        userAuthentication.getOwnProfileData()
+            .then(function (data) {
+                userAuthentication.saveUserData(data);
+                $scope.ownProfileData = data;
+                $scope.ownProfileData.profileImage = 'data:image/jpeg;base64,' + data.profileImageData;
+                console.log(data);
+            }, function (data) {
+                console.log(data);
+            });
+    };
+
+    $scope.loadProfileData = function () {
+
+        // todo consider using the sessionStorage
+        $scope.getOwnProfileData();
     };
 });
