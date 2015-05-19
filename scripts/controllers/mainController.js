54261f5c97eacf77ca6a7db607f899f1d3cc339a
ivo <ivostoianov94@gmail.com>
2015-05-19 22:39:55 +0300

    Refactor

--- scripts/controllers/mainController.js
+++ scripts/controllers/mainController.js
@@ -2,11 +2,4 @@
 
 socialNetwork.controller('mainController', function ($scope) {
 
-    // todo see if this is good practise
-    /*if (sessionStorage.username) {
-    $scope.ownProfileData = JSON.parse(sessionStorage.ownProfileData);
-    $scope.ownProfileData.profileImage = 'data:image/jpeg;base64,' + $scope.ownProfileData.profileImage;
-}*/
-
-    //console.log($scope.ownProfileData);
 });
