a25241fd426e1aae2d98085dc799bb830479b021
ivo <ivostoianov94@gmail.com>
2015-05-21 00:05:24 +0300

    Implemet notify service

--- scripts/services/notifyService.js
+++ scripts/services/notifyService.js
@@ -1,5 +1,35 @@
-/* socialNetwork */
+/* global socialNetwork, noty */
 
-socialNetwork.factory('notifyService', function ($scope) {
+socialNetwork.factory('notifyService', function () {
+    var notification = {};
 
+    $.noty.defaults.killer = true;
+    $.noty.defaults.animation = {
+        open: {
+            height: 'toggle'
+        },
+        close: {
+            height: 'toggle'
+        },
+        easing: 'swing',
+        speed: 500
+    };
+    $.noty.defaults.layout = 'topCenter';
+    $.noty.defaults.timeout = 1900;
+
+    notification.success = function (message) {
+        noty({
+            text: message,
+            type: 'success'
+        });
+    };
+
+    notification.error = function (errorMessage) {
+        noty({
+            text: errorMessage,
+            type: 'error'
+        });
+    };
+
+    return notification;
 });
