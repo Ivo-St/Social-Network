58376ce7d4ae7ee5ee4c2a8d80962e6eb190cd7e
ivo <ivostoianov94@gmail.com>
2015-05-21 00:04:30 +0300

    Remove comment

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,6 @@
 
 var socialNetwork = angular.module('SocialNetwork', ['ngRoute']);
 
-// future: move the constants to another file
 socialNetwork.constant('BASE_URL', 'http://softuni-social-network.azurewebsites.net/api/');
 
 socialNetwork.config(function ($routeProvider) {
