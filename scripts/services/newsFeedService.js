5489a57bd7c4ddd0a8f33a26d41ab41793ca3451
ivo <ivostoianov94@gmail.com>
2015-05-23 23:36:24 +0300

    Create function for getting the news feed by pages

--- scripts/services/newsFeedService.js
+++ scripts/services/newsFeedService.js
@@ -1,5 +1,27 @@
-/* global socialNetwork */
+/* global socialNetwork, sessionStorage */
 
-socialNetwork.factory('newsFeedService', function () {
+socialNetwork.factory('newsFeedService', function ($http, $q, BASE_URL) {
+    var feedService = {};
 
+    feedService.getNewsFeed = function () {
+        var deferred = $q.defer();
+
+        var request = {
+            method: 'GET',
+            url: BASE_URL + 'me/feed?StartPostId&PageSize=5',
+            headers: {
+                Authorization: 'Bearer ' + sessionStorage.accessToken
+            }
+        };
+
+        $http(request)
+            .success(function (data) {
+                deferred.resolve(data);
+            })
+            .error(function (data) {
+                deferred.reject(data);
+            });
+
+        return deferred.promise;
+    };
 });
