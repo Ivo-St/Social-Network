c012b5ed66be66fe8798b2ff5e5e9a07462b30ad
ivo <ivostoianov94@gmail.com>
2015-05-25 22:48:06 +0300

    Create user service

---
+++ scripts/services/userService.js
@@ -0,0 +1,26 @@
+/* global socialNetwork, sessionStorage */
+
+socialNetwork.factory('userService', function ($http, $q, BASE_URL) {
+    var userService = {};
+
+    userService.getUserFullData = function (username) {
+        var deferred = $q.defer();
+        var url = BASE_URL + 'users/' + username;
+        var headers = {
+            Authorization: 'Bearer ' + sessionStorage.accessToken
+        };
+
+        $http.get(url, {
+                headers: headers
+            })
+            .success(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+
+        return deferred.promise;
+    };
+
+    return userService;
+});
