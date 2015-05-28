d3228467683b38550b51c9f05d664c45d56237ee
ivo <ivostoianov94@gmail.com>
2015-05-28 18:45:10 +0300

    Create header controller

---
+++ scripts/controllers/headerController.js
@@ -0,0 +1,49 @@
+/* global socialNetwork */
+
+socialNetwork.controller('headerController', function ($scope, friendsService, notifyService) {
+    function getFriendRequestIndexById(id) {
+        var index = -1;
+        for (var i in $scope.friendsRequests) {
+            if ($scope.friendsRequests[i].id == id) {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    $scope.getFriendRequests = function () {
+        friendsService.getFriendsRequests()
+            .then(function (data) {
+                $scope.friendsRequests = data;
+            }, function (data) {
+                notifyService.error('An error occured. ' + data.message);
+            });
+    };
+
+    $scope.acceptFriendRequest = function (id) {
+        friendsService.acceptFriendRequest(id)
+            .then(function (data) {
+                notifyService.success('Friend request approved successfully.');
+                var index = getFriendRequestIndexById(id);
+                $scope.friendsRequests[index].splice(index, 1);
+            }, function (data) {
+                notifyService.error('An error occured. ' + data.message);
+            });
+    };
+
+    $scope.declineFriendRequest = function (id) {
+        friendsService.acceptFriendRequest(id)
+            .then(function (data) {
+                notifyService.success('Friend request declined successfully.');
+                var index = getFriendRequestIndexById(id);
+                $scope.friendsRequests.splice(index, 1);
+            }, function (data) {
+                notifyService.error('An error occured. ' + data.message);
+            });
+    };
+
+    $scope.getOwnProfileData();
+    $scope.getFriendRequests();
+});
